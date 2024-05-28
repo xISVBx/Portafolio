00929509c9c9020102ea432792bb88d629da8a16
Ivan Santiago Vasquez Ballesteros <ivansantiagovb@gmail.com>
2024-05-28 18:01:23 -0500

    disenio responsive para el general

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ function App() {
   return (
     <>
       <div className='flex justify-center w-full bg-bg'>
-        <div className='pl-16 pr-4 pt-4 relative w-full max-w-[1000px]'>
+        <div className='sm:pl-16 sm:pr-4 sm:pt-4 relative w-full max-w-[1000px]'>
           {/* El body, sobre este trabajo todo*/}
-          <div className="bg-primary rounded-2xl bg-clip-border backdrop-filter backdrop-blur-xl bg-opacity-100 relative z-20" style={{ backgroundImage: `url(${textura})` }}>
+          <div className="bg-primary sm:rounded-2xl bg-clip-border backdrop-filter backdrop-blur-xl bg-opacity-100 relative z-20" style={{ backgroundImage: `url(${textura})` }}>
             <Portada />
             {/* El cuerpo del contenido*/}
-            <div className='pt-10 mb-5 px-10 bg-bg-light rounded-b-2xl'>
+            <div className='pt-10 sm:mb-5 px-10 bg-bg-light sm:rounded-b-2xl'>
               <AcercaDeMi />
               {/* Habilidades y Servicios*/}
               <div className='w-full flex justify-center  flex-col'>
@@ -33,8 +33,8 @@ function App() {
           </div>
 
 
-          <div className='h-[calc(50vh - 2.5rem)] bg absolute bg-primary-light ml-10 mt-10 mr-16  inset-0 z-10 rounded-2xl bg-clip-border backdrop-filter backdrop-blur-xl bg-opacity-50'></div>
-          <div className='h-[calc(50vh - 2.5rem)] bg absolute bg-primary-extraLight ml-4 mt-16 mr-16  inset-0 z-5 rounded-2xl bg-clip-border backdrop-filter backdrop-blur-xl bg-opacity-70'></div>
+          <div className='sm:h-[calc(50vh - 2.5rem)] bg absolute bg-primary-light ml-10 mt-10 mr-16  inset-0 z-10 rounded-2xl bg-clip-border backdrop-filter backdrop-blur-xl bg-opacity-50'></div>
+          <div className='sm:h-[calc(50vh - 2.5rem)] bg absolute bg-primary-extraLight ml-4 mt-16 mr-16  inset-0 z-5 rounded-2xl bg-clip-border backdrop-filter backdrop-blur-xl bg-opacity-70'></div>
         </div>
       </div>
     </>
